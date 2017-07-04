98860a173f990b451b5d7ad352a9ab92f9755560
Keita Tomochika <tomochika.keita.tz1@is.naist.jp>
2017-07-04 19:11:35 +0900

    [add]add benchmarck function for optimization (Beale function)

--- python/benchmark_func.py
+++ python/benchmark_func.py
@@ -10,7 +10,6 @@ from mpl_toolkits.mplot3d import Axes3D
 ##### Class Ackley function #####
 class Ackley:
     def __init__(self, variable_num):
-        ### Information each particle ###
         self.variable_num = variable_num
         self.max_search_range = 32.768
         self.min_search_range = -32.768
@@ -47,7 +46,6 @@ class Ackley:
 ##### Class Sphere function #####
 class Sphere:
     def __init__(self, variable_num):
-        ### Information each particle ###
         self.variable_num = variable_num
         self.max_search_range = 1000 # nearly inf
         self.min_search_range = -1000 # nearly inf
@@ -84,7 +82,6 @@ class Sphere:
 ##### Class Rosenbrock function #####
 class Rosenbrock:
     def __init__(self, variable_num):
-        ### Information each particle ###
         self.variable_num = variable_num
         self.max_search_range = 5
         self.min_search_range = -5
@@ -121,11 +118,49 @@ class Rosenbrock:
         else:
             print('This method only can use for 2 variables')
 
+##### Class Beale function #####
+class Beale:
+    def __init__(self):
+        self.variable_num = 2
+        self.max_search_range = 4.5
+        self.min_search_range = -4.5
+        self.optimal_solution = np.array([3,0.5])
+
+    def get_optimal_solution(self):
+        return self.optimal_solution
+
+    def get_search_range(self):
+        return [self.max_search_range, self.min_search_range]
+
+    def get_func_val(self, variables):
+        tmp1 = np.power(1.5 - variables[0] + variables[0] * variables[1],2)
+        tmp2 = np.power(2.25 - variables[0] + variables[0] * np.power(variables[1],2),2)
+        tmp3 = np.power(2.625 - variables[0] + variables[0] * np.power(variables[1],3),2)
+        return tmp1+tmp2+tmp3
+
+    def plot_2dimension(self):
+        if self.variable_num == 2:
+            x = np.arange(self.min_search_range,self.max_search_range, 0.25)
+            y = np.arange(self.min_search_range,self.max_search_range, 0.25)
+            X, Y = np.meshgrid(x,y)
+            Z = []
+            for xy_list in zip(X,Y):
+                z = []
+                for xy_input in zip(xy_list[0],xy_list[1]):
+                    z.append(self.get_func_val(np.array(xy_input)))
+                Z.append(z)
+            Z = np.array(Z)
+            fig = plt.figure()
+            ax = Axes3D(fig)
+            ax.plot_wireframe(X,Y,Z)
+            plt.show()
+        else:
+            print('This method only can use for 2 variables')
+
 ##### Class Goldstein-Price function #####
 class GoldsteinPrice:
-    def __init__(self, variable_num):
-        ### Information each particle ###
-        self.variable_num = variable_num
+    def __init__(self):
+        self.variable_num = 2
         self.max_search_range = 2
         self.min_search_range = -2
         self.optimal_solution = np.array([0,-1])
@@ -162,7 +197,7 @@ class GoldsteinPrice:
 
 
 def main():
-    benchmark_func = GoldsteinPrice(2)
+    benchmark_func = Beale()
     benchmark_func.plot_2dimension()
 
 if __name__ == '__main__':
